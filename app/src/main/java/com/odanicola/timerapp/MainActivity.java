6b67c6c03dbae5657b4d6baf5f0d6baed9267e25
Oda Aditiya Nicola <odanicola@gmail.com>
2019-01-23 11:36:44 +0700

    update

--- app/src/main/java/com/odanicola/timerapp/MainActivity.java
+++ app/src/main/java/com/odanicola/timerapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.odanicola.timerapp;
 
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new CountDownTimer(10000, 1000){
+          public  void onTick(long l){
+            //CountDown is counting down (every second)
+              Log.i("Seconds left", String.valueOf(l/1000));
+          }
+
+          public void onFinish(){
+            //Counter is finished (after 10 second)
+              Log.i("Timer is ", "finished");
+          }
+        }.start();
 
         /*
         final Handler handler = new Handler();
